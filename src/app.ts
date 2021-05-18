168870088e9db9997f7ee8197d14dc76c9b0f874
Sumilokhande1502 <75734154+Sumilokhande1502@users.noreply.github.com>
2021-05-18 17:02:49 +0530

    updates

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import df from './default/default';
 import route from './routes/router';
 
 const PORT = process.env.PORT || 5000;
-const server_host = process.env.localhost || '0.0.0.0';
+const HOST = process.env.HOST || '0.0.0.0';
 
 const uri = df.uri as string;
 
@@ -26,4 +26,4 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true,useFindA
 })
 
 
-app.listen(5000, () => console.info(`Server is running at http://${server_host}:${PORT}`));
+app.listen(5000, () => console.info(`Server is running at http://${HOST}:${PORT}`));
